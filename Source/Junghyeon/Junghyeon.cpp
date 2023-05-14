1e7d164806b868c66e9643eec00f5b700c15e1b2
Junghyeon <sie08357@naver.com>
2023-05-14 11:45:50 +0900

    초기 커밋.

---
+++ Source/Junghyeon/Junghyeon.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Junghyeon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Junghyeon, "Junghyeon" );
---
+++ Source/Junghyeon/JunghyeonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JunghyeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUNGHYEON_API AJunghyeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
